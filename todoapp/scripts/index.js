f6b3f8a5d44d6e9cb1fa502d780a03b4ff23bb61
Kapil Shrestha <sht.kapil@gmail.com>
2023-11-29 13:27:59 +0545

    feat: added to completed

--- todoapp/scripts/index.js
+++ todoapp/scripts/index.js
@@ -6,19 +6,27 @@ function addTask() {
     alert("No Tasks written");
   } else {
 
-    let li = document.createElement("li");
-
-    
-    
-    
+    let li = document.createElement("li");   
     
     li.textContent = inputBox.value;
-    
+
     let checkBox = document.createElement('input');
     checkBox.type = 'checkbox';
     checkBox.className= 'circular__checkbox';
+
+
     li.appendChild(checkBox);
-    console.log(li)
+    
+
+    checkBox.addEventListener('change', function() {
+      if (checkBox.checked) {
+        document.getElementById('task-fields-completed').appendChild(li)
+      } else {
+        document.getElementById('task-fields-remaining').removeChild(li)
+      }
+    });
+    
+    console.log(checkBox)
 
     // Insert the new li before the first child of taskFieldContainer
     if (taskFieldContainer.firstChild) {
@@ -33,3 +41,18 @@ function addTask() {
 
 const addButton = document.getElementById('form-btn');
 addButton.addEventListener('click', addTask);
+
+
+
+document.getElementById("completed-btn").addEventListener("click", function() {
+  document.getElementById("task-fields-container").classList.remove("active");
+  document.getElementById("task-fields-completed").classList.add("active");
+  document.getElementById("task-fields-remaining").classList.remove("active");
+});
+
+document.getElementById("remaining-btn").addEventListener("click", function() {
+  document.getElementById("task-fields-container").classList.remove("active");
+  document.getElementById("task-fields-completed").classList.remove("active");
+  document.getElementById("task-fields-remaining").classList.add("active");
+});
+
