68ba9a3bead124812db66605891bcdf4a4edcdd8
Kapil Shrestha <sht.kapil@gmail.com>
2023-12-06 00:20:39 +0545

    add: whiteboard

---
+++ whiteboard/src/scripts/constants.js
@@ -0,0 +1,16 @@
+// const canvas = document.getElementById('drawing-board');
+// const toolbar = document.getElementById('toolbar');
+// const ctx = canvas.getContext('2d');
+
+// const canvasOffsetX = canvas.offsetLeft;
+// const canvasOffsetY = canvas.offsetTop;
+
+// canvas.width = window.innerWidth - canvasOffsetX;
+// canvas.height = window.innerHeight - canvasOffsetY;
+
+// export let isPainting = false;
+// export let lineWidth = 5;
+// let startX;
+// let startY;
+
+// export {canvas, toolbar, ctx, canvasOffsetX, canvasOffsetY, canvas.width, canvas.height, isPainting, lineWidth, startX, startY}
