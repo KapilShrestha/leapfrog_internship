263bf5d494dd81a358200cec562057209a73c0c9
Kapil Shrestha <sht.kapil@gmail.com>
2023-12-14 11:40:26 +0545

    fix: minor changes in tools

--- whiteboard/src/scripts/tools.js
+++ whiteboard/src/scripts/tools.js
@@ -1,5 +1,6 @@
 colorPickerTool.addEventListener("click", function (e) {
   document.body.classList.remove("eraser__default");
+   document.body.classList.remove("text__default");
   document.body.classList.add("pen__default");
   isPenActive = true;
   isErasing = false;
@@ -13,6 +14,7 @@ penTool.addEventListener("click", function (e) {
   isHandSelected = false;
   isTextToolSelected = false;
   document.body.classList.remove("eraser__default");
+  document.body.classList.remove("text__default");
   document.body.classList.add("pen__default");
 
   slider.style.display = "block"; //displays only when penTool is selected
@@ -30,8 +32,10 @@ eraserButton.addEventListener("click", function () {
   isPenActive = false;
   isHandSelected = false;
   isTextToolSelected = false;
+  document.body.classList.remove("hand__default");
   document.body.classList.remove("pen__default");
   document.body.classList.add("eraser__default");
+  document.body.classList.remove("text__default");
   slider.style.display = "block"; //displays only when eraserTool is selected
   if (isErasing) {
     return;
@@ -70,25 +74,68 @@ slider.oninput = function () {
   }
 };
 
-
 handTool.addEventListener("click", function (e) {
-    isPenActive = false;
-    isErasing = false;
-    if (isHandSelected) {
-      return;
-    }
-    canvas.addEventListener('wheel', zoom);
-    canvas.addEventListener("click", function (e) {
-      console.log(e, "retest")
-      // console.log(e.clientX, e.clientY);
-      curves.map((c) => {
-        console.log(c)
-        if (c.minX < e.clientX < c.maxX && c.minY < e.clientY < c.maxY) {
-          drawBoundingBox(c.minX, c.minY, c.maxX, c.maxY);
-        }
-      });
+  isPenActive = false;
+  isErasing = false;
+  document.body.classList.remove("pen__default");
+  document.body.classList.remove("eraser__default");
+  document.body.classList.remove("text__default");
+  document.body.classList.add("hand__default")
+  if (isHandSelected) {
+    return;
+  }
+  canvas.addEventListener("wheel", zoom);
+  canvas.addEventListener("click", function (e) {
+    curves.map((c) => {
+      console.log(c);
+      if (c.minX < e.clientX < c.maxX && c.minY < e.clientY < c.maxY) {
+        drawBoundingBox(c.minX, c.minY, c.maxX, c.maxY);
+        document.body.classList.add("hand__grabbing")
+      }
     });
-  
-    isHandSelected = !isHandSelected;
-    handTool.style.backgroundColor = "red";
   });
+
+  isHandSelected = !isHandSelected;
+  // handTool.style.backgroundColor = "red";
+});
+
+// textboxtool
+
+textBoxTool.addEventListener("click", function (e) {
+  isPenActive = false;
+  isErasing = false;
+  isHandSelected = false;
+
+  document.body.classList.remove("pen__default");
+  document.body.classList.remove("eraser__default");
+  document.body.classList.remove("hand__default");
+  document.body.classList.add("text__default");
+  if (isTextToolSelected) {
+    return;
+  }
+  isTextToolSelected = true;
+});
+
+function createTextBox() {
+  canvas.addEventListener("click", function (event) {
+    var x = event.clientX - canvas.getBoundingClientRect().left;
+    var y = event.clientY - canvas.getBoundingClientRect().top;
+
+    var input = document.createElement("input");
+    input.type = "text";
+    input.style.position = "absolute";
+    input.style.left = x + "px";
+    input.style.top = y + "px";
+    input.style.minWidth = "50px"; // Set a minimum width
+
+    document.body.appendChild(input);
+
+    input.addEventListener("input", function () {
+      var text = input.value;
+      ctx.font = window.getComputedStyle(input).getPropertyValue("font");
+      var textWidth = ctx.measureText(text).width + 5; // To add some extra space
+      input.style.width = textWidth + "px"; // To adjust width based on text content
+    });
+    input.focus(); // Set focus to the newly created text box
+  });
+}
