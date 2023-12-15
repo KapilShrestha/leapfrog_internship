810d03d65cd61e889dd4610a09b45d605ea40821
Kapil Shrestha <sht.kapil@gmail.com>
2023-12-15 15:25:34 +0545

    revert git

--- whiteboard/src/scripts/textbox.js
+++ whiteboard/src/scripts/textbox.js
@@ -1,32 +1,32 @@
-// textBoxTool.addEventListener("click", function (e) {
-//   isPenActive = false;
-//   isErasing = false;
-//   if (isHandSelected) {
-//     return;
-//   }
-//   isTextToolSelected = true;
-// });
+textBoxTool.addEventListener("click", function (e) {
+  isPenActive = false;
+  isErasing = false;
+  if (isHandSelected) {
+    return;
+  }
+  isTextToolSelected = true;
+});
 
-// function createTextBox() {
-//   canvas.addEventListener("click", function (event) {
-//     var x = event.clientX - canvas.getBoundingClientRect().left;
-//     var y = event.clientY - canvas.getBoundingClientRect().top;
+function createTextBox() {
+  canvas.addEventListener("click", function (event) {
+    var x = event.clientX - canvas.getBoundingClientRect().left;
+    var y = event.clientY - canvas.getBoundingClientRect().top;
 
-//     var input = document.createElement("input");
-//     input.type = "text";
-//     input.style.position = "absolute";
-//     input.style.left = x + "px";
-//     input.style.top = y + "px";
-//     input.style.minWidth = "50px"; // Set a minimum width
+    var input = document.createElement("input");
+    input.type = "text";
+    input.style.position = "absolute";
+    input.style.left = x + "px";
+    input.style.top = y + "px";
+    input.style.minWidth = "50px"; // Set a minimum width
 
-//     document.body.appendChild(input);
+    document.body.appendChild(input);
 
-//     input.addEventListener("input", function () {
-//       var text = input.value;
-//       ctx.font = window.getComputedStyle(input).getPropertyValue("font");
-//       var textWidth = ctx.measureText(text).width + 5; // To add some extra space
-//       input.style.width = textWidth + "px"; // To adjust width based on text content
-//     });
-//     input.focus(); // Set focus to the newly created text box
-//   });
-// }
+    input.addEventListener("input", function () {
+      var text = input.value;
+      ctx.font = window.getComputedStyle(input).getPropertyValue("font");
+      var textWidth = ctx.measureText(text).width + 5; // To add some extra space
+      input.style.width = textWidth + "px"; // To adjust width based on text content
+    });
+    input.focus(); // Set focus to the newly created text box
+  });
+}
---
+++ whiteboard/src/scripts/upload.js
@@ -0,0 +1,25 @@
+// window.onload = function() {
+    
+
+//   uploadImage.addEventListener('change', function(e) {
+//     const file = e.target.files[0];
+//     const reader = new FileReader();
+
+//     reader.onload = function(event) {
+//       const img = new Image();
+
+//       img.onload = function() {
+//       //   canvas.width = img.width;
+//       //   canvas.height = img.height;
+
+//       img.width =canvas.width;
+//       img.height = canvas.height;
+//         ctx.drawImage(img, 0, 0);
+//       };
+
+//       img.src = event.target.result;
+//     };
+
+//     reader.readAsDataURL(file);
+//   });
+// };
