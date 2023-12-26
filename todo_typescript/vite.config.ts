2ebc1d9c61789198faad5d3fc8dd88eeeaf36f6b
Kapil Shrestha <sht.kapil@gmail.com>
2023-12-26 13:23:16 +0545

    fix: vite config base added

--- todo_typescript/vite.config.ts
+++ todo_typescript/vite.config.ts
@@ -3,10 +3,11 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: './',
     resolve: { alias: { '@': '/src' } },
     // plugins: [vue()],
     server: {
         host: true,
         port: 3000
-    }
+    } 
 })
